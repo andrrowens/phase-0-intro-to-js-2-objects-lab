956f0ad127a6200530765dd5c8693b49cab333c1
Andrew Owens <andrrowens@gmail.com>
2022-11-26 20:26:14 -0700

    commit tree c1356be9582a373efc294eaef2a49f43404f4c6d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: "John Jones",
+    streetAddress: "123 John Street Denver, CO 80220",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...employee}
+    newObj[key] = value
+    return newObj
+}
+
+updateEmployeeWithKeyAndValue(employee, "Sam", "11 Broadway")
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "Sam", "12 Broadway")
+
+
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = delete newObj.name
+    return newEmployee
+}
